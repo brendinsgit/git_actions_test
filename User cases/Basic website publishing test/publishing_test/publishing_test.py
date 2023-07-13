cb9400e309ce14d7b6192e2121c50545ca76d3c8
Max Gapa <max.gapa@kenja.com>
2023-07-13 10:09:44 +0200

    Revert "Removed cache files, added git ignore"
    This reverts commit 06bb0d9e7dc8326abaa52ccfdef627b2fc4bb08a.

--- User cases/Basic website publishing test/publishing_test/publishing_test.py
+++ User cases/Basic website publishing test/publishing_test/publishing_test.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+import os
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
