3dbaba98be614c91296367cd5b1d1e3eedc4b90a
Maksymilian <98607717+YderSu@users.noreply.github.com>
2023-10-31 14:16:40 +0100

    Update flow_create.py

--- UserCases/flow_create.py
+++ UserCases/flow_create.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import (
     NoSuchElementException,
     ElementNotInteractableException,
