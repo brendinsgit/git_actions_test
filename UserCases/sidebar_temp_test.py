36649ab1247b1b50cc75ff29e5e27ff146986a47
brendinsgit <139824521+brendinsgit@users.noreply.github.com>
2023-11-19 18:00:16 -0500

    .

--- UserCases/sidebar_temp_test.py
+++ UserCases/sidebar_temp_test.py
@@ -285,9 +285,11 @@ class Sidebar_template_test:
             EC.invisibility_of_element_located((By.CLASS_NAME, "modal-backdrop"))
         )
         print("Time to publish the room...")
-        self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "glyphicon-globe"))
-        ).click()
+        
+        element = self.wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, "glyphicon-globe"))
+        )
+        self.driver.execute_script("arguments[0].click();", element)
 
         self.wait.until(
             EC.element_to_be_clickable(
