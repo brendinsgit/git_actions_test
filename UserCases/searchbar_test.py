15e4522be10b3157e1f8d2c97053fefd603c8354
brendinsgit <139824521+brendinsgit@users.noreply.github.com>
2023-11-27 16:05:02 -0500

    .

--- UserCases/searchbar_test.py
+++ UserCases/searchbar_test.py
@@ -10,6 +10,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
